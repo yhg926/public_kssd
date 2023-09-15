d9c9e295a0762b63b2567c09a457ac21c54df279
yhg926 <yhg926@gmail.com>
2023-09-15 09:57:54 +0800

    minor

--- src/abv_match_metaData.pl
+++ src/abv_match_metaData.pl
@@ -1,18 +1,18 @@
 use warnings;
 use diagnostics;
 if(@ARGV!=2){
-	die "*.pl <abv search output> <All_metag.run_sample_orgnism.cs>";
+	die "*.pl <abv search output> <All_metag.run_proj_sample_orgnism.cs>";
 }
 
 open $meta,$ARGV[1] || die "cant open $ARGV[1]:$!";
 $head=<$meta>;
 chomp $head;
-($colname2,$colname3)=(split /\,/,$head)[1,2];
+($colname2,$colname3,$colname4)=(split /\,/,$head)[1,2,3];
 while(<$meta>){
 	chomp;
-	($run,$bsmp,$orgnism) = (split /\,/);
+	($run,$bpj,$bsmp,$orgnism) = (split /\,/);
 	if((defined $bsmp) and (defined $orgnism)) {
-		$hash{$run} = $bsmp."\t".$orgnism;
+		$hash{$run} = $bpj."\t".$bsmp."\t".$orgnism;
 	}
 }
 close($meta);
@@ -23,13 +23,13 @@ while(<$abv>){
 	chomp;
 	($abvname,$measure)=(split /\t/);
 	if ($abvname !~ /\.abv$/){
-		print $abvname,"\t",$measure,"\t",$colname2,"\t",$colname3,"\n";
+		print $abvname,"\t",$measure,"\t",$colname2,"\t",$colname3,"\t",$colname4,"\n";
 	}
 	else{
 		$abvname=~s/\.abv$//;
 		print $abvname,"\t",$measure,"\t";
 		if(!exists $hash{$abvname}){
-			print "NA\tNA\n";
+			print "NA\tNA\tNA\n";
 		}
 		else {
 			print $hash{$abvname},"\n";
